bb45f3340e26f52d7f9a7863ff21c8453a8fd1d9
purps <goglqbmo@gmail.com>
2021-11-18 16:34:55 +0300

    fixed tsconfig now it inlcudes ./src/**/*

---
+++ prisma/seed.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield prisma.catalogue.createMany({
+            data: [
+                { name: '🧥 Верхняя одежда', url: 'luckybuyer888_kurtki' },
+                { name: '👞 Обувь', url: 'luckybuyer888_shoes' },
+            ],
+        });
+    });
+}
+main()
+    .catch((e) => {
+    console.error(e);
+    process.exit(1);
+})
+    .finally(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$disconnect();
+}));
+//# sourceMappingURL=seed.js.map
