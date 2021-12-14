37680151a3c0819b44433aa51cc06a07958db7f8
unknown <Student@R-5-12.itstep.local>
2021-12-14 22:10:01 +0300

    added style to lesson-link

--- js/lessons/lesson-1.js
+++ js/lessons/lesson-1.js
@@ -8,4 +8,17 @@ if(!false) {
     console.log('Local f', f);
 }
 
-console.log(f)
+for(let i = 0; i < 5; i++)
+    showIndex(i)
+
+function showIndex(i) {
+    console.log(i)
+}
+
+const person = {
+    name: 'John',
+    action: 'wants to watch'
+}
+
+const cnt = Array.from(['Wonder Woman', 'Spider-man: No Way Home', 'Godzilla'], (i) => `I want to watch ${i}`)
+console.log(cnt);
